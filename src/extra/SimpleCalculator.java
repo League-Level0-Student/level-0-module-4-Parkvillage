15a93ca1a06f51883d1d8f2d27cab25029998b35
League <League@administorsimac.attlocal.net>
2018-12-22 13:01:28 -0800

    Did half of mod. 4 checkpoint

--- src/extra/SimpleCalculator.java
+++ src/extra/SimpleCalculator.java
@@ -4,23 +4,44 @@ package extra;
 import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
+	
+	public static int addition(int n1, int n2) {
+		return n1+n2;
+	
+	}
+
+	public static void subtraction(int n1, int n2) {
+		JOptionPane.showMessageDialog(null, n1+"-"+n2+"="+(n1+n2));
+	}
+	
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
-		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+      String num1=JOptionPane.showInputDialog(null, "Welcome to the simple calculator, choose a number");
+      String num2=JOptionPane.showInputDialog(null, "choose another number");	// 2. Customize pop-up to support add/subtract/multiply/divide operations.
+      												 																	
+		int operation = JOptionPane.showOptionDialog(null, "what operation would you like to possess", "choose an operation", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "addition", "subtraction", "multiplicatuin", "division" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
 
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
+		if(operation==0) {
+		int a=	addition(Integer.parseInt(num1),Integer.parseInt(num2));
+			JOptionPane.showMessageDialog(null, a);
+		}
+		
+		else if(operation==1) {
+			JOptionPane
+		}
+		}
+			subtraction(Integer.parseInt(num1),Integer.parseInt(num2));
 	}
+
 
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
 
-	// 4. Create similar methods for subtraction, multiplication and division.
 }
